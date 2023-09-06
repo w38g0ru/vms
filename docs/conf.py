2883ad0303d8e219e52f213d74550ee0b7cc8daf
Anish Bhattarai <w38g0ru@anish.com.np>
2023-09-06 22:20:51 +0545

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,11 @@
 # conf.py
+import sphinx_rtd_theme
+
 project = 'Vacancy Management System Documentation'
 author = 'Anish Bhattarai'
 master_doc = 'index'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
+    'sphinx_rtd_theme',
 ]
